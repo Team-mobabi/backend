14e4c420b5de8f1cc3bc52591a5e552ecf02d763
pingmong <kny7024@naver.com>
2025-10-09 13:31:56 +0900

    fix: cors 주소 변경

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      "https://www.wisoft.io",
+      "https://daisy.wisoft.io",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
