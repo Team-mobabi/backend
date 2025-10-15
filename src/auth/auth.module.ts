33be858770228932d34941dded705f0b44691aa7
pingmong <kny7024@naver.com>
2025-10-16 00:19:12 +0900

    타입 체크 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { JwtModule, JwtModuleOptions } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import type { StringValue } from "ms";
 import { AuthService } from "@src/auth/auth.service";
 import { AuthController } from "@src/auth/auth.controller";
 import { UsersModule } from "@src/users/users.module";
@@ -15,12 +16,15 @@ import { JwtStrategy } from "@src/auth/strategies/jwt.strategy";
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService): JwtModuleOptions => ({
-        secret: configService.get<string>("JWT_SECRET") || "default-secret",
-        signOptions: {
-          expiresIn: configService.get<string>("JWT_EXPIRES_IN") || "1d",
-        },
-      }),
+      useFactory: (configService: ConfigService): JwtModuleOptions => {
+        const expiresIn = configService.get<string>("JWT_EXPIRES_IN") || "1d";
+        return {
+          secret: configService.get<string>("JWT_SECRET") || "default-secret",
+          signOptions: {
+            expiresIn: expiresIn as StringValue,
+          },
+        };
+      },
     }),
   ],
   controllers: [AuthController],
